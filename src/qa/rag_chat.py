d92a6f29af4f9cfacb7e0a3e453f7185652c9ef4
KarnatakaManaswini <145191117+KarnatakaManaswini@users.noreply.github.com>
2026-05-11 19:15:23 +0530

    Update rag_chat.py

--- src/qa/rag_chat.py
+++ src/qa/rag_chat.py
@@ -1,61 +1,25 @@
-'''from openai import OpenAI
+from google import genai
+from dotenv import load_dotenv
 import os
 
-
-def get_client():
-    api_key = os.getenv("OPENAI_API_KEY")
-    if not api_key:
-        raise ValueError("OPENAI_API_KEY not found")
-    return OpenAI(api_key=api_key)
+load_dotenv()
 
 
 def generate_answer(question: str, context_chunks: list[str]) -> str:
-    client = get_client()
 
-    context = "\n\n".join(context_chunks)
-
-    prompt = f"""
-You are a helpful study assistant.
-Answer the question using ONLY the context below.
-If the answer is not present, say "Answer not found in the document."
-
-Context:
-{context}
-
-Question:
-{question}
-
-Answer:
-"""
-
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": prompt}],
-        temperature=0.2
-    )
-
-    return response.choices[0].message.content.strip()
-
-
-
-
-'''
-import google.generativeai as genai
-import os
-
-
-def generate_answer(question: str, context_chunks: list[str]) -> str:
     api_key = os.getenv("GEMINI_API_KEY")
 
     if not api_key:
         return "Error: GEMINI_API_KEY not found"
 
-    genai.configure(api_key=api_key)
+    try:
+        client = genai.Client(api_key=api_key)
 
-    context = "\n\n".join(context_chunks)
+        context = "\n\n".join(context_chunks)
 
-    prompt = f"""
+        prompt = f"""
 You are a helpful study assistant.
+
 Answer the question using ONLY the context below.
 
 If the answer is not present, say:
@@ -70,16 +34,12 @@ Question:
 Answer:
 """
 
-    try:
-        model = genai.GenerativeModel("gemini-1.5-flash")
-        response = model.generate_content(prompt)
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt
+        )
 
-        if response.text:
-            return response.text.strip()
-        return "No response generated."
+        return response.text
 
     except Exception as e:
         return f"Gemini failed: {str(e)}"
-    
-
-    
